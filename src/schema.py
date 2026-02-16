0ea953c52db9979ffe3f89996ee504dbc9953fd8
Leandro Gross <lgross@Leandros-MacBook-Pro.local>
2026-02-16 11:33:05 -0300

    feat: schema.py updated adding global class config

--- src/schema.py
+++ src/schema.py
@@ -1,8 +1,13 @@
-from pydantic import BaseModel, Field, EmailStr, field_validator
+from pydantic import BaseModel, Field, EmailStr, field_validator, ConfigDict
 from typing import Literal
 from datetime import date
 
 class SolicitudProducto(BaseModel):
+
+    # CONFIGURACIÓN GLOBAL DE LA CLASE
+    # Esto aplica trimming (strip) a TODOS los campos de texto automáticamente
+    model_config = ConfigDict(str_strip_whitespace=True)
+
     id_solicitud: str
     fecha_solicitud: date  # Pydantic parseará strings a fechas automáticamente
     tipo_producto: str
@@ -20,4 +25,4 @@ class SolicitudProducto(BaseModel):
     @field_validator('tipo_producto')
     @classmethod
     def normalizar_producto(cls, v):
-        return v.upper().strip() # Regla: Normalización
+        return v.upper() # Regla: Normalización
